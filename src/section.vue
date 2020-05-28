2d3b8ccda5087e7d804e47c16a682e5c1c65cdc1
borisAtWeweb <boris@weweb.io>
2020-05-28 10:07:50 +0200

    fix animation and responsive navigator

--- src/section.vue
+++ src/section.vue
@@ -5,6 +5,23 @@
 <!-- This is your HTML -->
 <template>
   <div class="section-container">
+    <svg style="display: none"
+         xmlns="http://www.w3.org/2000/svg">
+      <defs>
+        <svg id="arrow"
+             width="10"
+             height="16"
+             viewBox="0 0 10 16"
+             fill="none"
+             xmlns="http://www.w3.org/2000/svg">
+          <path fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M8.05984 0.986755C7.95984 0.886755 7.83317 0.840088 7.7065 0.840088C7.57984 0.840088 7.45317 0.886755 7.35317 0.986755L0.686504 7.65342C0.493171 7.84676 0.493171 8.16009 0.686504 8.35342L7.35317 15.0201C7.5465 15.2134 7.85984 15.2134 8.05317 15.0201L9.3065 13.7668C9.39984 13.6734 9.45317 13.5468 9.45317 13.4134C9.45317 13.2801 9.39984 13.1534 9.3065 13.0601L4.2465 8.00009L9.31317 2.94675C9.4065 2.85342 9.45984 2.72675 9.45984 2.59342C9.45984 2.46009 9.4065 2.33342 9.31317 2.24009L8.05984 0.986755ZM7.7065 2.03342L8.25984 2.58675L3.19984 7.65342C3.0265 7.82675 3.0065 8.09342 3.13984 8.28675C3.14984 8.29675 3.15984 8.30842 3.16984 8.32009C3.17984 8.33175 3.18984 8.34342 3.19984 8.35342L8.25984 13.4134L7.7065 13.9668L1.73984 8.00009L7.7065 2.03342Z"
+                fill="#5D5D5D"/>
+        </svg>
+
+      </defs>
+    </svg>
     <!-- wwManager:start -->
     <wwSectionEditMenu :sectionCtrl="sectionCtrl"></wwSectionEditMenu>
     <!-- wwManager:end -->
@@ -63,12 +80,18 @@
         </li>
       </ul>
       <ul class="testimonial-navigator">
-        <li v-for="(testimonial) in section.data.testimonials"
+        <li class="testimonial-navigator-arrow"
+            @click="selectPrevious()">
+          <svg>
+            <use href="#arrow"/>
+          </svg>
+        </li>
+        <li v-for="(testimonial,index) in section.data.testimonials"
             :key="testimonial.uniqueId"
-            @click="toggleTestimonial(testimonial)"
         >
           <svg class="testimonial-navigator-item"
                :class="{selected:isTestimonialSelected(testimonial)}"
+               @click="selectTestimonialAt(index,true)"
                width="24"
                height="12"
                viewBox="0 0 24 12"
@@ -78,6 +101,12 @@
                   height="12"
                   rx="4"
                   fill="#FDC806"/>
+          </svg>
+        </li>
+        <li class="testimonial-navigator-arrow right"
+            @click="selectNext(true)">
+          <svg>
+            <use href="#arrow"/>
           </svg>
         </li>
       </ul>
@@ -107,6 +136,7 @@
                 startTime: 0,
                 duration: 5000
             },
+            timer: undefined,
             elemOptions: {
                 items: [
                     {
@@ -142,6 +172,9 @@
             },
             editMode () {
                 return this.sectionCtrl.getEditMode() === 'CONTENT';
+            },
+            interactionsLocked () {
+                return this.timer !== undefined;
             }
         },
         watch: {
@@ -179,27 +212,6 @@
                 }
                 needUpdate && this.update();
             },
-            playLoop () {
-                const { testimonials } = this.section.data;
-                this.animation.id = requestAnimationFrame(() => {
-                    const delta = new Date() - this.animation.startTime;
-                    if (delta >= LOOP_DURATION) {
-                        const length = testimonials.length;
-                        const nextIdx = testimonials.indexOf(this.selectedTestimonial) + 1;
-                        this.selectedTestimonial = this.section.data.testimonials[nextIdx >= length ? 0 : nextIdx];
-                        this.animation.startTime = new Date();
-                    }
-                    this.playLoop();
-                });
-            },
-            stopLoop () {
-                cancelAnimationFrame(this.animation.id);
-                this.animation = {
-                    ...this.animation,
-                    id: 0,
-                    startTime: 0
-                };
-            },
             getNewTestimonial: () => ({
                 uniqueId: wwu.getUniqueId(),
                 text: wwo.getDefault({ type: 'ww-text' }),
@@ -217,20 +229,50 @@
                 return this.getNewTestimonial();
             },
             isTestimonialSelected (testimonial) {
-                return testimonial.uniqueId === this.selectedTestimonial.uniqueId;
+                return !!testimonial && testimonial.uniqueId === this.selectedTestimonial.uniqueId;
             },
-            toggleTestimonial (testimonial) {
-                this.stopLoop();
-                this.selectedTestimonial = testimonial;
-                if (this.editMode) return;
-                this.restartLoop();
+            selectPrevious () {
+                const currentIdx = this.section.data.testimonials.indexOf(this.selectedTestimonial);
+                const nextIdx = currentIdx > 0 ? currentIdx - 1 : this.section.data.testimonials.length - 1;
+                this.selectTestimonialAt(nextIdx, true);
+            },
+            selectNext (pauseLoop = false) {
+                const length = this.section.data.testimonials.length;
+                const currentIdx = this.section.data.testimonials.indexOf(this.selectedTestimonial);
+                const nextIdx = currentIdx < length - 1 ? currentIdx + 1 : 0;
+                this.selectTestimonialAt(nextIdx, pauseLoop);
+            },
+            selectTestimonialAt (idx, pauseLoop = false) {
+                this.selectedTestimonial = this.section.data.testimonials[idx];
+                this.$forceUpdate();
+                if (pauseLoop) this.restartLoop();
+            },
+            playLoop () {
+                this.animation.id = requestAnimationFrame(() => {
+                    const delta = new Date() - this.animation.startTime;
+                    if (delta >= LOOP_DURATION) {
+                        this.selectNext();
+                        this.animation.startTime = new Date();
+                    }
+                    this.playLoop();
+                });
+            },
+            stopLoop () {
+                cancelAnimationFrame(this.animation.id);
+                this.animation = {
+                    ...this.animation,
+                    id: 0,
+                    startTime: 0
+                };
             },
             restartLoop () {
-                this.$forceUpdate();
-                const timer = setTimeout(() => {
+                this.stopLoop();
+                if (this.timer) clearTimeout(this.timer);
+                this.timer = setTimeout(() => {
+                    clearTimeout(this.timer);
+                    this.timer = void 0;
                     this.playLoop();
-                    clearTimeout(timer);
-                }, LOOP_DURATION);
+                }, 3000);
             },
             addTestimonialBefore (index) {
                 this.addTestimonialAt(index === 0 ? 0 : index - 1);
@@ -245,8 +287,7 @@
                 const head = testimonials.slice(0, index);
                 const tail = index === 0 ? testimonials : testimonials.slice(index);
                 this.section.data.testimonials = [...head, this.createTestimonial(), ...tail];
-            }
-            ,
+            },
             removeTestimonial (testimonial) {
                 const { testimonials } = this.section.data;
                 if (testimonials.length === 1) return;
@@ -279,8 +320,8 @@
             }
             /* wwManager:end */
         }
-    }
-    ;
+    };
+
 </script>
 
 <!-- This is your CSS -->
@@ -439,6 +480,11 @@
           width: 24px;
           height: 4px;
           rx: 0;
+
+          &:hover {
+            transition: all 250ms;
+            fill: var(--color-yellow);
+          }
         }
 
         &.selected {
@@ -446,15 +492,38 @@
           margin-top: 0;
 
           rect {
-            fill: var(--color-yellow);;
+            fill: var(--color-yellow);
             width: 12px;
             height: 12px;
             rx: 4px;
           }
         }
       }
-    }
 
+      &-arrow {
+        display: block;
+        width: 20px;
+        height: 20px;
+        margin-right: 24px;
+        overflow: hidden;
+        pointer-events: all;
+
+        @media (min-width: 768px) {
+          display: none;
+        }
+
+        &.right {
+          transform: rotateZ(180deg) translateY(5px);
+        }
+      }
+    }
+  }
+
+  .stopTransitions * {
+    -webkit-transition: none !important;
+    -moz-transition: none !important;
+    -o-transition: none !important;
+    transition: none !important;
   }
 
 </style>
